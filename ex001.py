2140df31ff3d7f4081dcc3532ccf2469fc5332ef
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-10-09 18:42:23 -0300

    ex001.py
    Condições alinhas

---
+++ ex001.py
@@ -0,0 +1,10 @@
+'''
+Condições alinhas
+'''
+time = str(input('Qual seu time ? ')).lower()
+if time == 'corinthians':
+  print('Sim você tem MUNDIAL Parabéns -_- ')
+elif time == 'palmeiras':
+  print('Não você não tem mundial ')
+else:
+  print('Não são tão importantes  ')
